73c72b236ebb874b106a1825d9cee128613bef6f
Marco Vidal García <marcovidal67@gmail.com>
2015-04-16 14:05:42 +0200

    App icon replaced

--- app/src/main/java/com/mvidalgarcia/pinmap/ws/impl/PinREST.java
+++ app/src/main/java/com/mvidalgarcia/pinmap/ws/impl/PinREST.java
@@ -16,15 +16,12 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 
 public class PinREST implements PinWS {
